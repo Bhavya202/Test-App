966125d00d9d19c6d86c309815fd4f0728c11fdf
Bhavya202 <76608764+Bhavya202@users.noreply.github.com>
2021-05-17 19:34:43 +0530

    Add files via upload

---
+++ js/question.js
@@ -0,0 +1,278 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is 12 multipiead by 2?",
+    answer: "24",
+    options: [
+      "36",
+      "24",
+      "12",
+      "None Of The Above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The product of 6 and 9 is:-",
+    answer: "54",
+    options: [
+      "64",
+      "56",
+      "42",
+      "54"
+    ]
+  },
+    {
+    numb: 3,
+    question: "From following which of the following is in ascending order?",
+    answer: "12>32>49>86",
+    options: [
+      "49>32>86>12",
+      "32>86>49>12",
+      "12>32>49>86",
+      "86>49>32>12"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The greatest two digit number is:-",
+    answer: "99",
+    options: [
+      "99",
+      "91",
+      "82",
+      "46"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The smallest two digit number is:-",
+    answer: "10",
+    options: [
+      "48",
+      "10",
+      "22",
+      "54"
+    ]
+  },
+  {
+      numb: 6,
+      question: "From following which is in descending order?",
+      answer: "96>84>78>69",
+      options: [
+        "84>96>78>69",
+        "69>78>84>96",
+        "78>69>84>96",
+        "96>84>78>69"
+      ]
+  },
+  {
+      numb: 7,
+      question: "From following which is in descending order?",
+      answer: "52>45>32>27",
+      options: [
+          "52>45>32>27",
+          "45>32>27>52",
+          "27>32>45>52",
+          "32>27>52>45"
+      ]
+  },
+  {
+      numb: 8,
+      question: "From following which is in ascending order?",
+      answer: "64<82<88<98",
+      options: [
+          "98<82<88<64",
+          "64<82<88<98",
+          "82<64<88<98",
+          "88<82<64<98"
+      ]
+  },
+  {
+    numb: 9,
+    question: "What is the difference between of 82 and 64?",
+    answer: "18",
+    options: [
+        "14",
+        "12",
+        "18",
+        "08"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What should be added in 45 to make 96",
+    answer: "51",
+    options: [
+        "51",
+        "42",
+        "31",
+        "56"
+    ]
+  },
+  {
+      numb: 11,
+      question: "What is thw sum of 82 and 96?",
+      answer: "178",
+      options: [
+          "182",
+          "178",
+          "172",
+          "166"
+      ]
+  },
+  {
+      numb: 12,
+      question: "Subtract 58 from 96",
+      answer: "38",
+      options: [
+          "36",
+          "44",
+          "38",
+          "42"
+      ]
+  },
+  {
+      numb: 13,
+      question: "The product of 4 and 8?",
+      answer: "32",
+      options: [
+          "36",
+          "40",
+          "42",
+          "32"
+      ]
+  },
+  {
+      numb: 14,
+      question: "The product of 4 and 5",
+      answer: "20",
+      options: [
+          "35",
+          "20",
+          "30",
+          "25"
+      ]
+  },
+  {
+      numb: 15,
+      question: "What should be added in 56 to make 138",
+      answer: "82",
+      options: [
+        "78",
+        "88",
+        "86",
+        "82",
+      ]
+  },
+  {
+      numb: 16,
+      question: "What is bigger 22 or 29?",
+      answer: "29",
+      options: [
+        "32",
+        "39",
+        "29",
+        "22",
+      ]
+  },
+  {
+    numb: 17,
+    question: "What will be the ones digit in the number 79?",
+    answer: "9",
+    options: [
+      "7",
+      "9",
+      "16",
+      "2",
+    ]
+  },
+  {
+    numb: 18,
+    question: "What will be number obtained on adding 5 and 3?",
+    answer: "8",
+    options: [
+      "9",
+      "5",
+      "2",
+      "8",
+    ]
+  },
+  {
+    numb: 19,
+    question: "11 added to 1 is:-",
+    answer: "12",
+    options: [
+      "12",
+      "22",
+      "24",
+      "11",
+    ]
+  },
+  {
+    numb: 20,
+    question: "Fourty Eight in figures is:-",
+    answer: "48",
+    options: [
+      "Fourty Eight",
+      "48",
+      "Fourty 8",
+      "4 Eight",
+    ]
+  },
+  {
+    numb: 21,
+    question: "Short Form of 300+20+8:-",
+    answer: "328",
+    options: [
+      "382",
+      "238",
+      "328",
+      "832",
+    ]
+  },
+  {
+    numb: 22,
+    question: "10 less than 90 is:-",
+    answer: "80",
+    options: [
+      "08",
+      "90",
+      "80",
+      "10",
+    ]
+  },
+  {
+    numb: 23,
+    question: "Short Form of 500+40+0 is:-",
+    answer: "540",
+    options: [
+      "540",
+      "504",
+      "405",
+      "450",
+    ]
+  },
+  {
+    numb: 24,
+    question: "10 less than 40 is:-",
+    answer: "30",
+    options: [
+      "03",
+      "27",
+      "30",
+      "72",
+    ]
+  },
+  {
+    numb: 25,
+    question: "The greatest three digit number:-",
+    answer: "999",
+    options: [
+      "998",
+      "999",
+      "1000",
+      "100",
+    ]
+  },
+];
